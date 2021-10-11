2e8784a0654dd68cdff59e9129df9417cb98aa7c
Ervan Andi <ervan.andi45@gmail.com>
2021-10-11 18:53:47 +0700

    Update Lat3DeretHuruf.java

--- j2_latihan01/Lat3DeretHuruf.java
+++ j2_latihan01/Lat3DeretHuruf.java
@@ -17,10 +17,10 @@ package j2_latihan01;
 import java.util.Scanner;
 public class Lat3DeretHuruf {
     public static void main(String[] args) {
-        Scanner tool_input = new Scanner (System.in);
+        Scanner toolInput = new Scanner (System.in);
         System.out.print("Silahkan masukkan nilai n : ");        
-        int n = tool_input.nextInt();
-        tool_input.close();
+        int n = toolInput.nextInt();
+        toolInput.close();
 
         for (int i=0; i<n; i++) System.out.print((char)(65+i) + " ");
 
