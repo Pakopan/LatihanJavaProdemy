59586f1f6098c2685debfb44f76caf07bb22a5d7
Ervan Andi <ervan.andi45@gmail.com>
2021-10-11 18:53:03 +0700

    Update Lat2DeretGenap.java

--- j2_latihan01/Lat2DeretGenap.java
+++ j2_latihan01/Lat2DeretGenap.java
@@ -6,8 +6,8 @@ public class Lat2DeretGenap {
     public static void main(String[] args) {
         Scanner tool_input = new Scanner (System.in);
         System.out.print("Silahkan masukkan nilai n : ");
-        int n = tool_input.nextInt();
-        tool_input.close();
+        int n = toolInput.nextInt();
+        toolInput.close();
 
         for (int i=0; i<n; i++) System.out.print(i*2+2 + " ");
     }
