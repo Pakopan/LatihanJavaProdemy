81a7fd828a1f13558a1c389cdc2b43ce02193fcf
Ervan Andi <ervan.andi45@gmail.com>
2021-10-11 18:55:00 +0700

    Update Lat5DeretHurufAngka.java

--- j2_latihan01/Lat5DeretHurufAngka.java
+++ j2_latihan01/Lat5DeretHurufAngka.java
@@ -17,10 +17,10 @@ package j2_latihan01;
 import java.util.Scanner;
 public class Lat5DeretHurufAngka {
     public static void main(String[] args) {
-        Scanner tool_input = new Scanner (System.in);
+        Scanner toolInput = new Scanner (System.in);
         System.out.print("Silahkan masukkan nilai n : ");
-        int n = tool_input.nextInt();
-        tool_input.close();
+        int n = toolInput.nextInt();
+        toolInput.close();
 
         char pola_huruf = 'A';
 
